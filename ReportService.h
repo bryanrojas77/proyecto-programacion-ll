17b102a4ef482249d71a169376853f8a0a760065
Jose Ramos <josealbertoramossaravia@gmail.com>
2026-05-01 12:16:16 -0600

    Parte 2 casi completa

---
+++ ReportService.h
@@ -0,0 +1,17 @@
+//
+// Created by eXtreme PC on 1/5/2026.
+//
+
+#ifndef REPORTSERVICE_H
+#define REPORTSERVICE_H
+
+#include <vector>
+#include <string>
+class Equipo;
+
+class ReportService{
+public:
+    void generar(int dia, const std::vector<Equipo*>& equipos);
+};
+
+#endif
---
+++ Simulador.h
@@ -0,0 +1,13 @@
+//
+// Created by eXtreme PC on 1/5/2026.
+//
+
+#ifndef SIMULADOR_H
+#define SIMULADOR_H
+#include <vector>
+#include <memory>
+
+
+
+
+#endif
