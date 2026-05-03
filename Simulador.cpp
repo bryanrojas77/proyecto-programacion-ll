4a31b68fbb0bf44245ffba351514caf100f3fa2b
Bryan <bryan.rojas.barrantes@est.una.ac.cr>
2026-05-02 21:01:40 -0600

    Simulador.cpp: cambios

--- Simulador.cpp
+++ Simulador.cpp
@@ -13,8 +13,6 @@ void Simulador::ejecutarSimulacion(vector<Equipo*>& equipos, int dias) {
 
     for (int d = 1; d <= dias; d++) {
 
-        cout << "\n--- Dia " << d << " ---\n";
-
         for (auto& eq : equipos) {
 
             int prob = rand() % 100;
@@ -43,11 +41,5 @@ void Simulador::ejecutarSimulacion(vector<Equipo*>& equipos, int dias) {
         }
 
         Ordenamiento::ordenar(equipos);
-
-        cout << "Top 3:\n";
-        for (int i = 0; i < 3 && i < equipos.size(); i++) {
-            cout << equipos[i]->getId() << " -> "
-                 << equipos[i]->calcularPrioridad() << endl;
-        }
     }
 }
